c818b597feda10337b9f1077d4eed8f5d936bb9c
hasibgalib44 <hasibgalib44@gmail.com>
2025-05-25 13:33:01 +0530

    Update and rename sample.js to sample1.js

--- sample.js
+++
@@ -1,9 +0,0 @@
-// This function adds two numbers and prints the result to the console.
-const addNumbers = () => {
-  const a = 21;
-  const b = 20;
-  const c = a + b;
-  console.log(c);
-};
-
-addNumbers(); // Call the function to execute the logic
